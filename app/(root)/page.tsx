758b83d4f43966eb1aac7fb349c02ad73407cdac
Dwiki Nuri Dhuha <dwiki.nuridhuha@gmail.com>
2025-10-07 09:22:42 +0700

    15 - sample product & product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@/components/ui/button"
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 const HomePage = () => {
-    return (
-        <div><Button>Check</Button></div>
-    );
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default HomePage;
