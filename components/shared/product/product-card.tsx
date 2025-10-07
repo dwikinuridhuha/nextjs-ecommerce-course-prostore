fbb19581a76e9b51b936d601d4b8e78455be1383
Dwiki Nuri Dhuha <dwiki.nuridhuha@gmail.com>
2025-10-08 05:47:40 +0700

    17 - product price

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import ProductPrice from "@/components/shared/product/product-price";
 
 type TProductCardProps = {
   product: any;
@@ -25,11 +26,11 @@ const ProductCard = ({ product }: TProductCardProps) => {
         <Link href={`/product/${product.slug}`}>
           <h2 className="text-sm font-medium">{product.name}</h2>
         </Link>
-        <div className="flex-beetwen gap-4">
+        <div className="flex-between gap-4">
           <p>{product.rating} Stars</p>
 
           {product.stock > 0 ? (
-            <p className="font-bold">${product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className="text-destructive">Out Of Stock</p>
           )}
